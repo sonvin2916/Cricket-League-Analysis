3915c982ac15ed1f083a515494ae2558b04a7815
sonvin2916 <sonalchougule07@gmail.com>
2020-08-04 12:44:10 +0530

    [Sonal]Added :Top batting averages of the cricketers

--- src/main/java/com/cricketanalysis/ICSVBuilder.java
+++ src/main/java/com/cricketanalysis/ICSVBuilder.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.util.Iterator;
 import java.util.List;
 
+
 public interface ICSVBuilder<E> {
     public Iterator<E> getCSVFileIterator(BufferedReader reader, Class csvClass);
     public List<E> getCSVFileList(BufferedReader reader, Class csvClass);
